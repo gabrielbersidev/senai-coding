41c8d64ccdeb2e9b8226534642f2b650aabd9aaf
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-03-10 10:30:22 -0300

    feat: complete exercises 01 (sum) and 02, with corresponding packages and classes

--- 1-semestre/java-projects/prjLista01/src/pctExc2/Exercicio02.java
+++ 1-semestre/java-projects/prjLista01/src/pctExc2/Exercicio02.java
@@ -1,5 +1,11 @@
 package pctExc2;
 
 public class Exercicio02 {
+    public static void main(String [] args) {
+        int num1 = 10;
+        int num2 = 3;
 
+        int subtracao = num1 - num2;
+        System.out.println("A subtração de " + num1 + " e " + num2 + " é: " + subtracao);
+    }
 }
