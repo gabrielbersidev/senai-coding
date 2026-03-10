55b1b63552708bf1859440d76be0e22e61c5ecd5
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-03-10 10:56:52 -0300

    style: fix indentation in exercise 10 file

--- 1-semestre/java-projects/prjLista01/src/pctExc10/Exercicio10.java
+++ 1-semestre/java-projects/prjLista01/src/pctExc10/Exercicio10.java
@@ -1,8 +1,9 @@
 package pctExc10;
 
 public class Exercicio10 {
-public static void main(String [] args) {
+    public static void main(String [] args) {
     double lado1 = 4.0;
     double lado2 = 5.0;
     System.out.println("O perímetro do retângulo com lados " + lado1 + " e " + lado2 + " é: " + (2 * (lado1 + lado2)));
+    }
 }
