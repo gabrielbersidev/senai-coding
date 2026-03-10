4214629cee771f405191afa7f96178bb210d5310
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-03-10 09:15:13 -0300

    feat: add increment and decrement examples

--- 1-semestre/java-projects/prjAula02/src/pctEx01/Exemplo01.java
+++ 1-semestre/java-projects/prjAula02/src/pctEx01/Exemplo01.java
@@ -4,6 +4,7 @@ public class Exemplo01 {
 	public static void main(String [] args) {
 		double num1 = 5;
 		double num2 = 2;
+		int num3 = 1;
 		
 		System.out.println("\nOperadores:");
 		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
@@ -11,6 +12,12 @@ public class Exemplo01 {
 		System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
 		System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
 		System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+		System.out.println("\nNúmero antes incremento: "+num3);
+		num3++;
+		System.out.println("Número após incremento: "+num3);
+		num3=1;
+		num3--;
+		System.out.println("Númer após decremento: "+num3);
 
 	}
 }
