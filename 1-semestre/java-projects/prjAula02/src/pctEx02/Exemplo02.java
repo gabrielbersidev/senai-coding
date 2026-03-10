07a7ab9a2af55eb93f8f2d03ba854edbd0d1d890
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-03-10 09:33:14 -0300

    feat: move from arithmetic operators to relational operators, adding comparison examples that return true or false

---
+++ 1-semestre/java-projects/prjAula02/src/pctEx02/Exemplo02.java
@@ -0,0 +1,17 @@
+package pctEx02;
+
+public class Exemplo02 {
+	public static void main(String[] args){
+		int a = 2;
+		int b = 3;
+		
+		System.out.println("Operadores Relacionais:");
+		System.out.println(a + " é igual a " + b + ":" + " " + (a==b)); // comparação de igualdade
+		System.out.println(a + " é diferente de " + b + ":" + " " +	(a!=b)); // comparação de diferença
+		System.out.println(a + " é maior que " + b + ":" + " " + (a>b)); // comparação de maior que
+		System.out.println(a + " é menor que " + b + ":" + " " + (a<b)); // comparação de menor que
+		System.out.println(a + " é maior ou igual a " + b + ":" + " " + (a>=b)); // comparação de maior ou igual a
+		System.out.println(a + " é menor ou igual a " + b + ":" + " " + (a<=b)); // comparação de menor ou igual a
+
+	}
+}
