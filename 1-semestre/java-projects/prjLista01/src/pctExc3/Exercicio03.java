53e9dcd0f81facddf2a7f742816b9e0efb4ab56b
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-03-10 10:33:35 -0300

    feat: complete exercises 01 (sum) and 02, with corresponding packages and classes

--- 1-semestre/java-projects/prjLista01/src/pctExc3/Exercicio03.java
+++ 1-semestre/java-projects/prjLista01/src/pctExc3/Exercicio03.java
@@ -1,5 +1,11 @@
 package pctExc3;
 
 public class Exercicio03 {
+    public static void main(String [] args) {
+        int num1 = 4;
+        int num2 = 6;
 
+        int multiplicacao = num1 * num2;
+        System.out.println("A multiplicação de " + num1 + " e " + num2 + " é: " + multiplicacao);
+    }
 }
