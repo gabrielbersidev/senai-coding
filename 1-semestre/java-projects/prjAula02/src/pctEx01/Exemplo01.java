70053452165062b41a2085af52e30875f827161f
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-03-10 08:08:17 -0300

    feat: explore logical operators, add main class performing sum and demonstrating string concatenation with result output

--- 1-semestre/java-projects/prjAula02/src/pctEx01/Exemplo01.java
+++ 1-semestre/java-projects/prjAula02/src/pctEx01/Exemplo01.java
@@ -1,5 +1,11 @@
 package pctEx01;
 
 public class Exemplo01 {
-
+	public static void main(String [] args) {
+		int num1 = 5;
+		int num2 = 2;
+		
+		System.out.println("\nOperadores:");
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+	}
 }
