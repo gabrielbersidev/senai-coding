4835bf3d9a6fdc9aadca73e5e04ba93cfe0a5a70
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-03-10 10:34:16 -0300

    feat: complete exercises 03 (multiplication) and 04 (division) with corresponding packages and classes

--- 1-semestre/java-projects/prjLista01/src/pctExc4/Exercicio04.java
+++ 1-semestre/java-projects/prjLista01/src/pctExc4/Exercicio04.java
@@ -1,5 +1,11 @@
 package pctExc4;
 
 public class Exercicio04 {
+    public static void main(String [] args) {
+        int num1 = 20;
+        int num2 = 5;
 
+        int divisao = num1 / num2;
+        System.out.println("A divisão de " + num1 + " e " + num2 + " é: " + divisao);
+    }
 }
