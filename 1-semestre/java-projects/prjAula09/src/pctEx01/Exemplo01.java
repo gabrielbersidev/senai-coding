7e8a3e3b90cc99572166ded45dabec97a712bbb2
Gabriel Bersi <gabrielbersidev@gmail.com>
2026-04-07 10:24:50 -0300

    Implementar exemplos de laço for para aula 09

---
+++ 1-semestre/java-projects/prjAula09/src/pctEx01/Exemplo01.java
@@ -0,0 +1,13 @@
+package pctEx01;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 5; i++) {
+            System.out.println("Flexão nº" + i);
+        }
+
+		System.out.println("Monstro terminou");
+   }
+}
---
+++ 1-semestre/java-projects/prjAula09/src/pctEx02/Exemplo02.java
@@ -0,0 +1,12 @@
+package pctEx02;
+
+public class Exemplo02 {
+
+	public static void main(String[] args) {
+		System.out.println("Tabuada do 5:");
+
+        for(int i=1; i<=10; i++){
+            System.out.println("5*" + i + " = " + (i*5));
+        }
+   }
+}
